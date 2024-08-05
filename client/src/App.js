b8e4436e499c995af4149932b0edbb7429536f33
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-05 15:33:07 -0700

    chore: set up basic functionality to make sure frontend can fetch from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const[backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then (
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
   return (
-    <div>App</div>
+    <div>
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
+    </div>
   )
 }
 
