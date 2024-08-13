04f85b2ab59eca94d1dd2872ab21037d50aa5216
Tim Crawley Jr <timcrawley@Tims-MacBook-Pro.local>
2024-08-13 00:21:19 -0700

    added more styling to register and login pages

--- front-end/src/pages/RegisterPage.js
+++ front-end/src/pages/RegisterPage.js
@@ -1,6 +1,7 @@
 export default function RegisterPage() {
   return (
-    <form action=''>
+    <form className='register'>
+      <h1>Register</h1>
       <input type='text' placeholder='username' />
       <input type='password' placeholder='password' />
       <button>Register</button>
